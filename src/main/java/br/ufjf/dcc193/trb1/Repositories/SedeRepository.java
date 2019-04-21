606e7c492244115b94d7dd31d7642ca32d4310d4
Alex Maroco <alexsmaroco@gmail.com>
2019-04-21 14:08:17 -0300

    Cria repositório para Sede

---
+++ src/main/java/br/ufjf/dcc193/trb1/Repositories/SedeRepository.java
@@ -0,0 +1,13 @@
+package br.ufjf.dcc193.trb1.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufjf.dcc193.trb1.Models.Sede;
+
+/**
+ * SedeRepository
+ */
+public interface SedeRepository extends JpaRepository<Sede, Number>{
+
+    
+}
