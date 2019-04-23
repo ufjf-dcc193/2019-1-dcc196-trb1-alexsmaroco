46c1e9ada7552a95e3a932018de95440298ca27e
Alex Maroco <alexsmaroco@gmail.com>
2019-04-23 20:57:39 -0300

    Adiciona método para editar e deletar uma Sede

--- src/main/java/br/ufjf/dcc193/trb1/Controllers/SedeController.java
+++ src/main/java/br/ufjf/dcc193/trb1/Controllers/SedeController.java
@@ -2,6 +2,8 @@ package br.ufjf.dcc193.trb1.Controllers;
 
 import java.util.List;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -20,16 +22,20 @@ public class SedeController {
     @Autowired
     SedeRepository sedeRepo;
 
-    @RequestMapping("sedes.html")
+    @RequestMapping(method = RequestMethod.GET, path = "sedes.html")
     ModelAndView listaSedes(){
         ModelAndView mv = new ModelAndView();
         mv.setViewName("sedes");
         List<Sede> repoRes = sedeRepo.findAll();
-        for(Sede s: repoRes) {
-            System.out.println(s.getNome_fantasia());
-        }
         mv.addObject("sedes", repoRes);
         return mv;
+    }
+
+    @RequestMapping(method = RequestMethod.POST, path = "sedes.html")
+    @Transactional
+    RedirectView deletaSede(@RequestParam Integer id){
+        sedeRepo.deleteById(id);
+        return new RedirectView("sedes.html");
     }
 
     @RequestMapping(method=RequestMethod.GET, path="novaSede.html")
