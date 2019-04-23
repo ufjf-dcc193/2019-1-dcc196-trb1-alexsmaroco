504ab693d3e134b367a0b2907bc5188f4d4b2b58
Alex Maroco <alexsmaroco@gmail.com>
2019-04-22 22:26:49 -0300

    Cria repositório para Atividade

---
+++ src/main/java/br/ufjf/dcc193/trb1/Repositories/AtividadeRepository.java
@@ -0,0 +1,10 @@
+package br.ufjf.dcc193.trb1.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.ufjf.dcc193.trb1.Models.Atividade;
+
+public interface AtividadeRepository extends JpaRepository<Atividade, Integer>{
+
+    
+}
