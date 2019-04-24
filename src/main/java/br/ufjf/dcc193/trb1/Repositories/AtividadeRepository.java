5f72327b80a4d88eb9ad203c8bc70288f0c21bcb
Alex Maroco <alexsmaroco@gmail.com>
2019-04-24 00:27:09 -0300

    Adiciona query para busca por chave da Sede

--- src/main/java/br/ufjf/dcc193/trb1/Repositories/AtividadeRepository.java
+++ src/main/java/br/ufjf/dcc193/trb1/Repositories/AtividadeRepository.java
@@ -1,10 +1,12 @@
 package br.ufjf.dcc193.trb1.Repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.ufjf.dcc193.trb1.Models.Atividade;
 
 public interface AtividadeRepository extends JpaRepository<Atividade, Integer>{
 
-    
+    List<Atividade> findByFkIdSede(Integer fkIdSede);
 }
