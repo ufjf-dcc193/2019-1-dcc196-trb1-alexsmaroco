26c7df0ba83926c4835f86b15a97ad23fa0803f3
Alex Maroco <alexsmaroco@gmail.com>
2019-04-23 23:05:48 -0300

    Adiciona query para encontrar membros de uma sede

--- src/main/java/br/ufjf/dcc193/trb1/Repositories/MembroRepository.java
+++ src/main/java/br/ufjf/dcc193/trb1/Repositories/MembroRepository.java
@@ -1,9 +1,12 @@
 package br.ufjf.dcc193.trb1.Repositories;
 
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.ufjf.dcc193.trb1.Models.Membro;
 
 public interface MembroRepository extends JpaRepository<Membro, Integer>{
-    
+    List<Membro> findByFkIdSede(Integer fkIdSede);
 }
