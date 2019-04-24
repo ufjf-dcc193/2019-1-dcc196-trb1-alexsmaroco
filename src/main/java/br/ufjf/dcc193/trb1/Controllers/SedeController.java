7b106eb8f07639b47eec8740df45230e3c401d25
Alex Maroco <alexsmaroco@gmail.com>
2019-04-24 00:25:34 -0300

    Adiciona remoção das atividades relacionadas a Sede a ser deletada e função para gerar o relatório de horas

--- src/main/java/br/ufjf/dcc193/trb1/Controllers/SedeController.java
+++ src/main/java/br/ufjf/dcc193/trb1/Controllers/SedeController.java
@@ -1,6 +1,8 @@
 package br.ufjf.dcc193.trb1.Controllers;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import javax.transaction.Transactional;
 
@@ -13,8 +15,10 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
+import br.ufjf.dcc193.trb1.Models.Atividade;
 import br.ufjf.dcc193.trb1.Models.Membro;
 import br.ufjf.dcc193.trb1.Models.Sede;
+import br.ufjf.dcc193.trb1.Repositories.AtividadeRepository;
 import br.ufjf.dcc193.trb1.Repositories.MembroRepository;
 import br.ufjf.dcc193.trb1.Repositories.SedeRepository;
 
@@ -26,6 +30,9 @@ public class SedeController {
 
     @Autowired
     MembroRepository membroRepo;
+
+    @Autowired
+    AtividadeRepository ativRepo;
 
     @RequestMapping(method = RequestMethod.GET, path = "sedes.html")
     ModelAndView listaSedes() {
@@ -42,6 +49,10 @@ public class SedeController {
         List<Membro> membros = membroRepo.findByFkIdSede(id);
         for(Membro m: membros) {
             membroRepo.deleteById(m.getId());
+        }
+        List<Atividade> ativ = ativRepo.findByFkIdSede(id);
+        for(Atividade a: ativ) {
+            ativRepo.deleteById(a.getId());
         }
         sedeRepo.deleteById(id);
         return new RedirectView("sedes.html");
@@ -74,5 +85,27 @@ public class SedeController {
         BeanUtils.copyProperties(sede, s, "id");
         sedeRepo.save(s);
         return new RedirectView("detalhesSede.html?id=" + s.getId());
+    }
+
+    @RequestMapping("relatorio.html")
+    ModelAndView gerarRelatorio() {
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("relatorio");
+        List<Sede> sedes = sedeRepo.findAll();
+        List<String[]> dadosRelat = new ArrayList<String[]>();
+        for(Sede s: sedes) {
+            List<Atividade> ativs = ativRepo.findByFkIdSede(s.getId());
+            Integer[] totalHoras = {0,0,0,0};
+            for(Atividade a: ativs) {
+                totalHoras[0]+=a.getHoras_assistencial();
+                totalHoras[1]+=a.getHoras_juridica();
+                totalHoras[2]+=a.getHoras_financeira();
+                totalHoras[3]+=a.getHoras_executiva();
+            }
+            String[] str = {s.getNome_fantasia(), totalHoras[0].toString(), totalHoras[1].toString(), totalHoras[2].toString(), totalHoras[3].toString()};
+            dadosRelat.add(str);
+        }
+        mv.addObject("dados", dadosRelat);
+        return mv;
     }
 }
